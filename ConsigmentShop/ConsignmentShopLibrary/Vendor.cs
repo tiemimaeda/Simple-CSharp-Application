a35540f7427f79e30fc72a3880954d0b366addbc
Priscila Tiemi Maeda <54248304+tiemimaeda@users.noreply.github.com>
2020-07-19 19:33:32 -0300

    Add files via upload

---
+++ ConsigmentShop/ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConsignmentShopLibrary
+{
+    public class Vendor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Commission { get; set; }
+        public decimal  PaymentDue { get; set; }
+
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} {1}- R${2}", FirstName, LastName, PaymentDue);
+            }
+        }
+
+        public Vendor()
+        {
+            Commission = .5;
+        }
+    }
+}
